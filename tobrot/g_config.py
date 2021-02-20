140f9971561aca866dc96e1ad87f0dd44e07c766
Pirated143 <71497650+Pirated143-Indian@users.noreply.github.com>
2021-02-20 18:13:14 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -6,7 +6,7 @@ class Config(Config):
     APP_ID = 1373938
     API_HASH = "c05a1fff8d688c198d4665f4c08016a2"
     OWNER_ID = 12682063966
-    AUTH_CHANNEL = [-10082786282972]
+    AUTH_CHANNEL = [-1001253546382, -1001331815943, -1001386365453, -1001493702344, -1001232259062]
     DESTINATION_FOLDER = "Mirror Bot" #Name of your folder read readme(not id of the folder)
     RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
